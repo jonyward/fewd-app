f88a8b7dbffb32742ed5bc712c11e43d21fcbe51
jonyward <jonyward@gmail.com>
2022-12-16 12:59:57 +0000

    testing data format for displaying multiple ingredients

--- src/data/data.js
+++ src/data/data.js
@@ -3,30 +3,22 @@ export const recipes = [
     id: '1',
     name: 'Pizza',
     description: 'Homemade margerita pizza, made with fresh dough, tomato sauce, margerita cheese topped with fresh basil',
-    ingredients: [
-        {name: 'Margerita', amount: 'something'},
-        {name: 'Tomato', amount: 'something'},
-        {name: 'Basil', amount: 'something'},
-        {name: 'Pizza Dough', amount: 'something'},
-        {name: 'Garlic', amount: 'something'},
-        {name: 'Olive oil', amount: 'something'},
-        {name: 'Flour', amount: 'something'}
-        ],
-    sList: 'no'
+    ingredient: 'Tomato, 20g',
+    ingredient: 'Basil, 20g',
+    ingredient: 'Pizaa Dough, 20g',
+    ingredient: 'Garlic, 20g',
+    ingredient: 'Olive Oil, 20g',
+    ingredient: 'Flour, 20g'
     },
     {
-        id: '1',
-        name: 'Coffee',
-        description: 'Homemade margerita pizza, made with fresh dough, tomato sauce, margerita cheese topped with fresh basil',
-        ingredients: [
-            {name: 'Margerita', amount: 'something'},
-            {name: 'Tomato', amount: 'something'},
-            {name: 'Basil', amount: 'something'},
-            {name: 'Pizza Dough', amount: 'something'},
-            {name: 'Garlic', amount: 'something'},
-            {name: 'Olive oil', amount: 'something'},
-            {name: 'Flour', amount: 'something'}
-            ],
-        sList: 'no'
-        }
+    id: '1',
+    name: 'Coffee',
+    description: 'Homemade margerita pizza, made with fresh dough, tomato sauce, margerita cheese topped with fresh basil',
+    ingredient: 'Tomato, 20g',
+    ingredient: 'Basil, 20g',
+    ingredient: 'Pizaa Dough, 20g',
+    ingredient: 'Garlic, 20g',
+    ingredient: 'Olive Oil, 20g',
+    ingredient: 'Flour, 20g'
+    }
 ]
